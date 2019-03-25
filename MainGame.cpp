d7fec406c135272fd124d2ed3b717ed9a6517c5b
Pham Hong Vinh <phamvinh257@gmail.com>
2019-03-25 20:58:35 +0700

    Review Logic code. Recovered main function

--- MainGame.cpp
+++ MainGame.cpp
@@ -18,7 +18,7 @@ const int FONT_HEIGHT = 8;
 const int  GAME_WIDTH = 48;
 const int GAME_HEIGHT = 78;
 
-const int BALL_RADIUS = 0;
+const int BALL_RADIUS = 1;
 const int BALL_HEIGHT = 3;
 
 const int SPACE_WIDTH = 5;
@@ -82,7 +82,7 @@ void onGameUpdate(float elapsedTime);
 int main(int argc, char* argv[]) {
 	Init();
 	Intro();
-	while ( 1 ) {
+	while ( Menu() ) {
 		ResetGame();
 		auto t1 = std::chrono::system_clock::now();
 		auto t2 = t1;
@@ -116,8 +116,8 @@ void Init() {
 	// TODO: Init AudioPLayer
 	// TODO: Load game asset
 	// Load Intro and Menu
-	//LoadSprite(Logo_Outline, "Bound-Console-Game/GameData/Logo/Logo_Outline.dat");
-	//LoadMenuData();
+	LoadSprite(Logo_Outline, "Bound-Console-Game/GameData/Logo/Logo_Outline.dat");
+	LoadMenuData();
 }
 
 /*
@@ -199,17 +199,21 @@ int Menu() {
 		case KEY_E + 32:
 		case KEY_E:
 			return 0;
+
 		case KEY_P + 32:
 		case KEY_P:
 			return 1;
+
 		case KEY_O + 32:
 		case KEY_O:
 			Options();
 			break;
+
 		case KEY_C + 32:
 		case KEY_C:
 			Credits();
 			break;
+
 		case KEY_H + 32:
 		case KEY_H:
 			Help();
@@ -228,16 +232,17 @@ int Menu() {
 void ResetGame() {
 	// Initialize global variables
 	gameOver = false;
-	ball.x = 1 / 2 * GAME_WIDTH;
-	ball.y = 1 / 3 * GAME_HEIGHT;
-	//@Leader : Section Heigth is just to know where to put the Obstacle in the first place. So if you change the Game Height, it won't appeared in weird position.
+	ball.x = 1 / 2.0f * GAME_WIDTH;
+	ball.y = 1 / 3.0f * GAME_HEIGHT;
+
+	//Section Heigth is just to know where to put the Obstacle in the first place. So if you change the Game Height, it won't appeared in weird position.
 	SectionHeight = ((float)GAME_HEIGHT / NUMBER_OF_WALLS + 1) + 5; 
 	
 	for (int i = 0; i < 3; i++)
 	{
 		Obstacle[i].spaceX = rand() % (GAME_WIDTH - SPACE_WIDTH);
 		Obstacle[i].spaceY = (i + 1)*SectionHeight;
-		while (Obstacle[i].spaceX < 3) //@leader: I dont know if this needed to be here, but i dont see any necessary. Because it rand from 1 to 48 - SpaceWidth.
+		while (Obstacle[i].spaceX < 3) // Your code. Your responsibility
 		{
 			Obstacle[i].spaceY = rand() % (GAME_WIDTH - SPACE_WIDTH);
 		}
@@ -270,14 +275,15 @@ void GameHandleInput() {
 
 // === PLAY LOGIC ===
 void ObstacleLogic(float fElapsedTime);
-void controlBall(float elapsedTime);
+void controlBall(float elapsedTime); 
+void Collision();
+void DrawLogic();
 
 void GameLogic(float elapsedTime) {
-	// TODO: Update ball
-	ObstacleLogic(elapsedTime);
-	// TODO: Check for collision, increase score or game over
-	// TODO: Update Game Camera
-	controlBall(float elapsedTime);
+	controlBall(elapsedTime); //@ThanhViet: Logic Error
+	ObstacleLogic(elapsedTime); //@ThanhUy: Logic error
+	//Collision(); // hide for the purpose of seeing bug
+	DrawLogic();
 }
 
 
@@ -337,12 +343,12 @@ void controlBall(float elapsedTime)
 
 void DrawLogic()
 {
-	if (ball.y > BALL_LIMIT*1.0)
+	if (ball.y > BALL_LIMIT*1.0f)
 	{ 
 		for (int i = 0; i < NUMBER_OF_WALLS; i++)
 		{
-			Obstacle[i].spaceY -= ball.y - BALL_LIMIT*1.0;
-			ball.y = BALL_LIMIT*1.0;
+			Obstacle[i].spaceY -= ball.y - BALL_LIMIT*1.0f;
+			ball.y = BALL_LIMIT*1.0f;
 		}
 	}
 }
@@ -381,7 +387,7 @@ void drawHUD() {
 
 void drawStage(int originX, int originY, int maxX, int maxY) {
 
-	ScreenBuffer::drawCircle(originX + ball.x + 0.5f, originY + ball.y + 0.5f, BALL_RADIUS, ' ', Color::BG_RED);
+	ScreenBuffer::fillRect(originX + ball.x-BALL_RADIUS + 0.5f, originY + ball.y-BALL_RADIUS + 0.5f, originX + ball.x + BALL_RADIUS + 0.5f, originY + ball.y + BALL_RADIUS + 0.5f,' ', Color::BG_RED);
 	for (int i = 0; i < NUMBER_OF_WALLS; i++) {
 		int drawSpaceX = Obstacle[i].spaceX + 0.5f;
 		int drawSpaceY = Obstacle[i].spaceY + 0.5f;
