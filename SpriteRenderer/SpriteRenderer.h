1915a1f71a792e1a717c70d66d4e8a14a51615c4
Trung <trantrung.trantrung.tt@gmail.com>
2019-04-05 14:38:29 +0700

    Add DrawCrop Function

--- SpriteRenderer/SpriteRenderer.h
+++ SpriteRenderer/SpriteRenderer.h
@@ -11,4 +11,5 @@ struct Sprite
 
 void LoadSprite(Sprite &sprite, std::string path);
 void DrawSprite(Sprite &sprite, int x, int y);
+void DrawCrop(Sprite &sprite, int x, int y, int x1, int y1, int x2, int y2);
 void FreeSprite(Sprite &sprite);
