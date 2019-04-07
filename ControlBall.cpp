872bef82c48902a8e96b53f44b57580db27aa2b7
Pham Hong Vinh <phamvinh257@gmail.com>
2019-04-08 00:44:45 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/Vintechnology/Bound-Console-Game""
    This reverts commit b3ee038a3ab20d82b3049dcb0acd7e000de12c7d.

--- ControlBall.cpp
+++ ControlBall.cpp
@@ -1,57 +1,50 @@
 #include "ControlBall.h"
 
-int next_space=0;
-int pre_space=0;
-int spacePressed;
-int leftPressed;
-int rightPressed;
 
-Ball ball;
+namespace ControlBall {
 
-void ControlAndUpdateBall(float elapsedTime)
-{
-	if (spacePressed)
-	{
-		ball.passed = 0;
-		ball.v = -g / 1.3f;
-		AudioPlayer::PlayEffect("Bound-Console-Game/GameData/Music/Jump.wav");
+	Ball* ball=nullptr;
+
+	void Jump() {
+		ball->velocityY = -g / 1.3f;
 	}
-	ball.y += ball.v*elapsedTime;
-	ball.v += g * elapsedTime;
 
-	if (leftPressed)
-		ball.x -= 10 * elapsedTime;
+	void GoLeft(float elapsedTime) {
+		ball->x -= 10 * elapsedTime;
+	}
 
-	if (rightPressed)
-		ball.x += 10 * elapsedTime;
-}
+	void GoRight(float elapsedTime) {
+		ball->x += 10 * elapsedTime;
+	}
 
-void LoadBall(Sprite &ballSkin, std::string path)
-{
-	LoadSprite(ballSkin, path);
-}
+	void Fall(float elapsedTime)
+	{
+		ball->y += ball->velocityY*elapsedTime;
+		ball->velocityY += g * elapsedTime;
+	}
 
-void DrawBall(Sprite &ballSkin, int originX, int originY)
-{
-	DrawSprite(ballSkin, originX + ball.x - BALL_RADIUS + 0.5f, originY + ball.y - BALL_RADIUS + 0.5f);
-}
+	void DrawBall(Sprite &ballSkin, int originX, int originY)
+	{
+		DrawSprite(ballSkin, originX + ball->x - BALL_RADIUS + 0.5f, originY + ball->y - BALL_RADIUS + 0.5f);
+	}
 
-void FreeBall(Sprite &ballSkin)
-{
-	FreeSprite(ballSkin);
-}
+	void ResetBall(float initX, float initY)
+	{
+		if (ball==nullptr)
+			ball = new Ball;
+		ball->x = initX;
+		ball->y = initY;
+		ball->velocityY = 0;
+		ball->velocityX = 0;
+	}
 
-void ResetBall()
-{
-	ball.x = 1 / 2.0f * GAME_WIDTH;
-	ball.y = 1 / 3.0f * GAME_HEIGHT;
-	ball.v = 0;
-	ball.passed = 0;
-}
 
-Ball* getBall()
-{
-	Ball *getBall;
-	getBall = &ball;
-	return getBall;
+	void deposeBall() {
+		delete ball;
+	}
+
+	Ball* getBall()
+	{
+		return ball;
+	}
 }
